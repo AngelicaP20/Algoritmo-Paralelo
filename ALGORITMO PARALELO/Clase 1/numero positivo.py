42df709d764eb529be7d1da058505e4c3dc6da69
AngelicaP20 <148393648+AngelicaP20@users.noreply.github.com>
2023-10-20 20:41:23 -0400

    Angelica Pérez 1-19-7588
    Repositorio sobre las clases de Algoritmo Paralelo.

---
+++ ALGORITMO PARALELO/Clase 1/numero positivo.py
@@ -0,0 +1,6 @@
+
+numero = int(input("Escriba un numero positivo"))
+if numero < 0:
+    print (f"El {numero} noes un numeropositivo")
+print (f"Ha escrito el numero {numero}")
+      
---
+++ ALGORITMO PARALELO/Clase 2/Op-opcion2.py
@@ -0,0 +1,18 @@
+op= int (input("Digite una opcion: "))
+
+match op:
+    case 1:
+        print("A")
+    case 2:
+        print("B")
+    case 2:
+        print("C")
+    case 3:
+        print("D")
+    case 4:
+        print("E")
+    case 5:
+        print("F")
+    case _:
+        print("Seleccione una opcion valida")
+        
