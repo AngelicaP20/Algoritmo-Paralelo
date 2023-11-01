33866c51a78589f2f18c0ff0ed8b4ff0467414ac
AngelicaP20 <148393648+AngelicaP20@users.noreply.github.com>
2023-11-01 18:20:29 -0400

    Add files via upload
    Angélica Pérez 1-19-7588

---
+++ Tarea_2/Lista_Sort().py
@@ -0,0 +1,5 @@
+#Crea una lista con 5 palabras. Luego, ordena alfabéticamente la lista con el método sort() y muestrala en pantalla.
+
+Palabras = ['Dado', 'Coche', 'Arbol', 'Estrella', 'Botella']
+Palabras.sort()
+print(Palabras)
