ca7f8fb5ae8172f2fccf1e80515af7eab2bb74c3
AngelicaP20 <148393648+AngelicaP20@users.noreply.github.com>
2023-11-01 18:26:39 -0400

    Angélica Pérez 1-19-7588

---
+++ ALGORITMO PARALELO/Tarea_2/Diccionario-Keys_None.py
@@ -0,0 +1,14 @@
+#Crea un diccionario con 3 keys y None como values. Luego modifica los values e imprime el diccionario.
+
+diccionario = {
+    'Corea del Sur': None,
+    'Ecuador': None,
+    'Francia': None
+}
+print(diccionario)
+
+diccionario['Corea del Sur'] = 'Seúl'
+diccionario['Ecuador'] =	'Quito'
+diccionario['Francia'] =	'París'
+
+print(diccionario)
